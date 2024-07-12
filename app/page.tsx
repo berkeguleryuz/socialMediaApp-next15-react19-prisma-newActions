0c36fdf2530df82613e60f69f71a84f82922ba8f
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-12 12:02:02 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
+import AddPost from "../components/AddPost";
+import Feed from "../components/Feed";
+import LeftMenu from "../components/LeftMenu";
+import RightMenu from "../components/RightMenu";
+import Stories from "../components/Stories";
+
 const Homepage = () => {
   return (
-    <div className='flex gap-6'>
+    <div className="flex gap-6">
       {/* LEFT */}
-      <div className="hidden xl:block"></div>
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu />
+      </div>
       {/* CENTER */}
-      <div className=""></div>
+      <div className="w-full lg:w-[70%] xl:[50%]">
+        <div className="flex flex-col gap-6">
+          <Stories />
+          <AddPost />
+          <Feed />
+        </div>
+      </div>
       {/* RIGHT */}
-      <div className="hidden lg:block"></div>
+      <div className="hidden lg:block">
+        <RightMenu />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
