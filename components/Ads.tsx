d178f92d74b25ad84e7c12557b789c7e77f10413
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-16 21:49:00 +0200

    update

--- components/Ads.tsx
+++ components/Ads.tsx
@@ -12,7 +12,10 @@ const Ads = ({ size }: { size: "sm" | "md" | "lg" }) => {
       {/* bottom */}
       <div
         className={`flex flex-col mt-4 ${size === "sm" ? "gap-2" : "gap-4"}`}>
-        <div className="relative">
+        <div
+          className={`relative w-full ${
+            size === "sm" ? "h-24" : size === "md" ? "h-36" : "h-48"
+          }`}>
           <Image
             src={
               "https://images.pexels.com/photos/25300367/pexels-photo-25300367/free-photo-of-peyzaj-manzara-alan-tarla.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
@@ -21,6 +24,18 @@ const Ads = ({ size }: { size: "sm" | "md" | "lg" }) => {
             className="rounded-lg object-cover"
             alt="image"
           />
+        </div>
+        <div className="flex items-center gap-4">
+          <Image
+            src={
+              "https://images.pexels.com/photos/25300367/pexels-photo-25300367/free-photo-of-peyzaj-manzara-alan-tarla.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            }
+            width={24}
+            height={24}
+            className="rounded-full w-6 h-6 object-cover"
+            alt="image"
+          />
+          <span className="text-blue-500 font-medium">Cool Lounge</span>
         </div>
       </div>
     </div>
