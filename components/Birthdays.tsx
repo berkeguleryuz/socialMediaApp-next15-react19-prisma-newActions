c7b7aa3f266ca63f9283d4451b3ab98054dca70d
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-16 14:53:39 +0200

    update

--- components/Birthdays.tsx
+++ components/Birthdays.tsx
@@ -15,7 +15,7 @@ const Birthdays = () => {
             src={
               "https://images.pexels.com/photos/19640282/pexels-photo-19640282/free-photo-of-balade-d-hi-ver.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
             }
-            alt=""
+            alt="image"
             width={100}
             height={100}
             className="w-10 h-10 rounded-full object-cover"
