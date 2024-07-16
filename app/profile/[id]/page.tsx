a6ed3351b133570cb7318e30f0b4507eb7e07042
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-17 00:08:56 +0200

    update

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -37,18 +37,18 @@ const ProfilePage = () => {
             <h1 className="mt-20 mb-4 text-2xl font-medium">Berke G</h1>
             <div className="flex items-center justify-center gap-12 mb-4">
               <div className="flex flex-col items-center">
-                <span className="font-medium">123</span>
+                <span className="font-medium">444</span>
                 <span className="text-sm">Posts</span>
               </div>
 
               <div className="flex flex-col items-center">
-                <span className="font-medium">123</span>
+                <span className="font-medium">777</span>
                 <span className="text-sm">Followers</span>
               </div>
 
               <div className="flex flex-col items-center">
-                <span className="font-medium">123</span>
-                <span className="text-sm">Posts</span>
+                <span className="font-medium">555</span>
+                <span className="text-sm">Following</span>
               </div>
             </div>
           </div>
