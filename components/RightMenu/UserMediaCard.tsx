4eae07a4f76fa7a8abf11baf175efe1bf015f3fd
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-18 11:59:45 +0200

    update

--- components/RightMenu/UserMediaCard.tsx
+++ components/RightMenu/UserMediaCard.tsx
@@ -1,9 +1,22 @@
+import prisma from "@/lib/client";
 import { User } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const UserMediaCard = ({ user }: { user: User }) => {
+const UserMediaCard = async ({ user }: { user: User }) => {
+  const postsWithMedia = await prisma.post.findMany({
+    where: {
+      userId: user.id,
+      img: {
+        not: null,
+      },
+    },
+    take: 8,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
       {/* TOP */}
@@ -15,86 +28,18 @@ const UserMediaCard = ({ user }: { user: User }) => {
       </div>
       {/* BOTTOM  */}
       <div className="flex gap-4 justify-between flex-wrap">
-        <div className="relative w-1/5 h-24">
-          <Image
-            src={
-              "https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-deniz-su-okyanus-kayalar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-24">
-          <Image
-            src={
-              "https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-deniz-su-okyanus-kayalar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-24">
-          <Image
-            src={
-              "https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-deniz-su-okyanus-kayalar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-24">
-          <Image
-            src={
-              "https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-deniz-su-okyanus-kayalar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-24">
-          <Image
-            src={
-              "https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-deniz-su-okyanus-kayalar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-24">
-          <Image
-            src={
-              "https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-deniz-su-okyanus-kayalar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-24">
-          <Image
-            src={
-              "https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-deniz-su-okyanus-kayalar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-24">
-          <Image
-            src={
-              "https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-deniz-su-okyanus-kayalar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
+        {postsWithMedia.length
+          ? postsWithMedia.map((post) => (
+              <div className="relative w-1/5 h-24" key={post.id}>
+                <Image
+                  src={post?.img!}
+                  alt=""
+                  fill
+                  className="object-cover rounded-md"
+                />
+              </div>
+            ))
+          : "No media found!"}
       </div>
     </div>
   );
